87828a2649dd7d94f05ffe598c102f1860c12658
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2021-03-07 11:12:37 -0300

    atualiza sobre na página index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { ToastProvider } from "react-toast-notifications";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { getRepos } from "../lib/repos";
 
 import Card from "../components/Card";
@@ -15,20 +16,27 @@ function HomePage({ repos }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖖</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖖🏼</text></svg>"
         />
       </Head>
 
       <ToastProvider>
-        <div className={styles.introducing}>
-          <h1 className={styles.introducingTitle}>
-            Olá, meu nome é<br />
-            <span>Jonatas Silva</span>
+        <div className={styles.about}>
+          <h1 className={styles.name}>
+            Olá, meu nome é <span>Jonatas Silva</span>
           </h1>
-          <p className={styles.introducingText}>
-            Estudante de Ciência da Computação no 5º período | 20 anos | Focado
-            em desenvolvimento web há 6 meses.
-          </p>
+          <h3 className={styles.bio}>
+            Estudante de Ciência da Computação.
+            <br /> Futuro Dev em T com foco no back-end.
+          </h3>
+          <div className={styles.social}>
+            <a href="https://github.com/jonatasosilva" target="_blank">
+              <FaGithub size={32} />
+            </a>
+            <a href="https://www.linkedin.com/in/jonatasosilva" target="_blank">
+              <FaLinkedin size={32} />
+            </a>
+          </div>
         </div>
 
         <div className={styles.repos}>
